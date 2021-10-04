259c42e91e79774ed9b2ac123ce605e273d149ee
ivantabera <ivantabera19@gmail.com>
2021-10-04 11:31:53 -0500

    Se generaron las vistas y las rutas

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import { AppRouter } from './routers/AppRouter';
 
 export const JournalApp = () => {
     return (
-        <div>
-            <h1>JournalApp12</h1>
-        </div>
+        <AppRouter/>
     )
-}
+};
