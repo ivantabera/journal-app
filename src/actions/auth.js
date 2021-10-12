660cd53b1237a94b9513b4a40c0c33156b227e21
ivantabera <ivantabera19@gmail.com>
2021-10-12 12:58:58 -0500

    Se genero el codigo para usar thunk middleware para realizar tareas que no son  sincronas

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,12 @@
-import { types } from "../types/types"
+import { types } from "../types/types";
+
+export const startLoginEmailPassword = (email, password) =>{
+    return ( dispatch ) => {
+        setTimeout(() => {
+            dispatch( login(123, 'Ivan') );
+        }, 3500);
+    }
+}
 
 export const login = ( uid, displayName ) => {
     return {
