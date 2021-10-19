690fb0b319843e02c9ae8bc633a441d6b57f364a
Ivan Tabera <ivantabera19@gmail.com>
2021-10-19 11:50:15 -0500

    Mantener el estado de la autenticacion por medio del hook useEffect al recargar

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect
 } from "react-router-dom";
+
+import '../firebase/firebase-config';
+
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        const auth = getAuth();
+
+        onAuthStateChanged(auth, (user) =>{
+            
+            if(user?.uid){
+
+                dispatch( login( user.uid, user.displayName ));
+            }
+        });
+
+    }, [dispatch]);
+
     return (
         <Router>
             <div>
