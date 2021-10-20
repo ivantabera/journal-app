4d3b80a7a2419cd877dbb4bcbe8a77da126eeb78
Ivan Tabera <ivantabera19@gmail.com>
2021-10-20 13:30:32 -0500

    Creacion de un reducer para las notas

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,30 @@
+/*
+    {
+        notes:[],
+        active: null,
+        active: {
+            id:'asfdlhkhq3kj5523',
+            title: '',
+            body: '',
+            imageUrl: '',
+            date: 14234123412
+        }
+    }
+ */
+
+const initialState = {
+    notes: [],
+    active: null
+}
+
+export const notesReducer = (state, action) => {
+
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
