4b3aadc85b107919e6f315966179291b6a25d352
Ivan Tabera <ivantabera19@gmail.com>
2021-11-02 12:04:07 -0600

    Se purgaron las notas en el logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,6 +4,7 @@ import { getAuth, signInWithPopup, createUserWithEmailAndPassword, updateProfile
 import { googleAuthProvider } from "../firebase/firebase-config";
 import { types } from "../types/types";
 import { finishLoading, startLoading } from './ui';
+import { noteLogout } from './notes';
 
 export const startLoginEmailPassword = (email, password) => {
     return ( dispatch ) => {
@@ -84,10 +85,11 @@ export const startLogout = () => {
         const auth = getAuth();
         await signOut( auth )
 
-        dispatch( logout() )
+        dispatch( logout() );
+        dispatch( noteLogout() );
     }
 };
 
 export const logout = () => ({
     type: types.logout
-})
+});
--- src/actions/notes.js
+++ src/actions/notes.js
@@ -118,3 +118,7 @@ export const deleteNote = ( id ) => ({
     type:types.notesDelete,
     payload:id 
 })
+
+export const noteLogout = () => ({
+    type: types.notesLogoutCleaning
+})
